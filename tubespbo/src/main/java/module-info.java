b697ab5f08a3aae661023c360f569a6d7cc4db27
Leona Rose <79490349+1972034leonarose@users.noreply.github.com>
2022-01-13 20:16:08 +0800

    Delete tubespbo directory

--- tubespbo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.ale.tubespbo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.ale.tubespbo to javafx.fxml;
-    exports com.ale.tubespbo;
-    opens com.ale.tubespbo.controller to javafx.fxml;
-}
